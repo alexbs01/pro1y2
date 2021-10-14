cec688e4076afb42afc2f0fa325e5c75712d2c0b
alexbs01 <alex.becerra.suarez01@gmail.com>
2021-10-14 14:05:42 +0200

    prueba se ignora

---
+++ src/boletin02/calculadora/cmake-build-debug/CMakeFiles/calculadora.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculadora.dir/main.c.obj"
+  "calculadora.exe"
+  "calculadora.exe.manifest"
+  "calculadora.pdb"
+  "libcalculadora.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculadora.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
