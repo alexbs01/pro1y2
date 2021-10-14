3e9108a921e440c615f4f0d030b8ef8092e8531c
alexbs01 <alex.becerra.suarez01@gmail.com>
2021-10-14 14:23:46 +0200

    Ejercicios nuevos añadidos

---
+++ src/boletin02/ejerciosBucles/bucleCuadradoRelleno/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int pisos;
+    char ladrillo;
+
+    do {
+        printf("Longitud del cuadrado "
+               "y que ladrillo usaras para el cuadrado (escribe una letra mayuscula y separa por espacio): ");
+        scanf("%d%*c%c", &pisos, &ladrillo);
+    } while ((pisos <= 0 || pisos >=51) ||(ladrillo < 'A' || ladrillo > 'Z'));
+   printf("\n");
+    for(int fila = 0; fila <= pisos; fila++) {
+        for (int i = 0; i <= pisos; i++) {
+            printf("%3c", ladrillo);
+        }
+        printf("\n");
+    }
+    return 0;
+}
