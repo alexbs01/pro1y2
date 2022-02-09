50d102fbb2d4bbca3e659d5c0186a6b5590eab71
alexbs01 <alex.becerra.suarez01@gmail.com>
2022-02-09 13:07:40 +0100

    Ya busca palabras en horizontal de izquierda a derecha

--- src/cosasPropias/resolvedorSopasDeLetras/main.c
+++ src/cosasPropias/resolvedorSopasDeLetras/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #define ENTER '\n'
@@ -8,25 +9,26 @@ void rellenarMatriz(int i, int j, char matriz[i][j], char elementos[i*j]);
 void mostrarMatriz(int i, int j, char matriz[i][j]);
 void buscarPalabra(int i, int j, char matriz[i][j], int longitud,  char palabra[longitud]);
 
+typedef char* pChar;
+
 int main() {
     int ancho = 15, alto = 15;
 
     printf("\nEscribe el ancho y el alto de la sopa de letras: ");
    // scanf("%d %d", &ancho, &alto);
     char letras[ancho*alto];
-    char palabra[ancho];
+    pChar palabra = malloc(sizeof(char));
     char sopa[ancho-1][alto-1]; //Crea una matriz con el tamano real que tendra la sopa
 
     //recogerLetras(ancho, alto, letras); //Recoge las letras de la sopa en un array unidimensional
 
-    rellenarMatriz(ancho, alto, sopa, "aholafrojoklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstadios"); //Las letras recogidas antes, se transforman en una matriz bidimensional
+    rellenarMatriz(ancho, alto, sopa, "aholafrojoklmnopqrstuazulabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstadios"); //Las letras recogidas antes, se transforman en una matriz bidimensional
     mostrarMatriz(ancho, alto, sopa); //Muestra la matriz con el formato de una sopa de letras
 
     printf("\nEscribe todas las palabras que deseas buscar separadas por espacios: ");
     do {
         scanf("%s", palabra);
-
-        buscarPalabra(ancho, alto, sopa, strlen(palabra), palabra);
+        buscarPalabra(ancho, alto, sopa, (int) strlen(palabra), palabra);
     } while(getchar() != ENTER);
 
     return 0;
@@ -68,50 +70,43 @@ void mostrarMatriz(int i, int j, char matriz[i][j]) {
     }
 }
 
-void buscarPalabra(int i, int j, char matriz[i][j], int longitud,  char palabra[longitud]) {
-    bool palabraEncontrada = false, letraCoincidente = false;
-    int contador = 0, fila = 0, columna = 0;
+void buscarPalabra(int i, int j, char matriz[i][j], int longitud, char palabra[longitud]) {
 
-    do {
+    int fila = 0, columna = 0;
+    pChar posiblePalabra = malloc(sizeof(char));
+     do {
         do {
-            if(matriz[columna][fila] == palabra[contador]) {
-                letraCoincidente = true;
-                contador++;
-            } else if((contador == strlen(palabra)) && (letraCoincidente == true)) {
-                palabraEncontrada = true;
-                printf("\nSe encontro la palabra en fila %d y columna %d", fila, (int) (columna) - (int) (strlen(palabra)));
-            } else {
-                contador = 0;
-                letraCoincidente = false;
+            //printf("\nEstoy fila %d y columna %d", fila, columna);
+            if(palabra[0] == matriz[columna][fila]) {
+                //printf("\nEstoy en IF en fila %d y columna %d, palabra[%d] %c matriz[%d][%d] %c\n", fila, columna, 0, palabra[0], columna, fila, matriz[columna][fila]);
+                for(int longitudPalabra = 0; longitudPalabra < longitud; longitudPalabra++) {
+                    posiblePalabra[longitudPalabra] = matriz[columna+longitudPalabra][fila];
+                    //printf("%c", posiblePalabra[longitudPalabra]);
+                }
+            }
+            if(strcmp(posiblePalabra, palabra)==0) {
+                break;
             }
 
             columna++;
-        } while((columna < i) || (palabraEncontrada != true));
+        } while(columna < i);
+         if(strcmp(posiblePalabra, palabra)==0) {
+             break;
+         }
         columna = 0;
-        contador = 0;
         fila++;
-    } while((fila < j) ||(palabraEncontrada != true));
+    } while(fila < j);
+
+     if(strcmp(posiblePalabra, palabra)==0) {
+         printf("\nSe encontro la palabra \"%s\" en fila %d y columna %d", palabra, fila+1, columna+1);
+     } else {
+         printf("\nNo se encontro la palabra \"%s\" en ninguna linea horizontal", palabra);
+     }
 
 
 
-   /* for (int fila = 0; (fila < j) || (palabraEncontrada == true); fila++) {
-        for (int columna = 0; (columna < i) || (palabraEncontrada == true); columna++) {
-            printf("\nLlegue hasta fila %d columna %d", fila, columna);
-            if(matriz[columna][fila] == palabra[0]) {
-                do {
-                    if(matriz[columna + contador][fila] != palabra[contador]) {
-                        palabraEncontrada = false;
-                    }
-                    printf("\nLlegue hasta aqui %d", contador);
-                    contador++;
-                } while(contador < longitud && palabraEncontrada == true);
-                if(palabraEncontrada == true) {
-                    printf("\nSe encontro la palabra la fila %d y columna %d", fila, columna);
-                }
-            }
-        }
-    }*/
-    printf("\nSe encontro la palabra");
+
+
 }
 
 //2 2 abcd
@@ -119,4 +114,4 @@ void buscarPalabra(int i, int j, char matriz[i][j], int longitud,  char palabra[
 //4 4 abcdefghijklmno
 //5 5 abcdefghijklmnopqrstuvwxy
 //10 10 abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy
-//15 15 aholafrojoklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstadios
+//15 15 aholafrojoklmnopqrstuazulabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstadios
